f0c8406367fa6a4b650d9ace20265d04379abacf
pan-dya <pandya.limawan@gmail.com>
2024-08-20 15:07:34 +1000

    fix eslint + deploy only on main

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App'; // Adjust the path according to where your App component is located
 // import '@testing-library/jest-dom/extend-expect';
 
