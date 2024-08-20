5d5e7140b4c026338dc6c1357e8344d17826f98a
pan-dya <pandya.limawan@gmail.com>
2024-08-21 01:37:50 +1000

    Added images, and update hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=yp6tRkS-QBU
-// 21.51
+// 31.22
 import React from 'react';
 import './Hero.css';
 
@@ -7,13 +7,47 @@ const Hero = () => {
   return (
     <section className="hero-container">
       <div className="hero-content">
-        <h2>Section Title</h2>
+        <h2>Hi, I&lsquo;m Pandya Limawan</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          animi similique inventore tempora soluta molestiae deleniti iure! Quas
-          illo omnis iste dolorem doloribus maxime accusantium, fugiat tempora
-          veritatis fugit? Rem?
+          I&lsquo;m an undergraduate student in Computer Science and Information
+          Technology working towards becoming a skilled software developer,
+          eager to take on real-world challenges
         </p>
+      </div>
+
+      <div className="hero-img">
+        <div className="profile-container">
+          <img src="/portfolio/assets/images/profile.jpg" alt="profile" />
+        </div>
+        <div className="tech-icon-grid">
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/react.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/express.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/django.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/bootstrap.svg" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/tailwind.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/mongodb.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/postgresql.png" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/github.jpg" alt="tech" />
+          </div>
+          <div className="tech-icon">
+            <img src="/portfolio/assets/images/vsc.png" alt="tech" />
+          </div>
+        </div>
       </div>
     </section>
   );
